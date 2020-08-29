0aa263c27731313bb90714d26d3b6a3e04f62be0
Ali Fakhry <ali.fakhry@fresh.com.eg>
2020-08-29 13:12:50 +0200

    Vue Router and VUEX Starter Boilerplate

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+import router from './appRouter';
+
+import { BootstrapVue } from 'bootstrap-vue';
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount('#app');
