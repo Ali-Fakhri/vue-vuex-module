8ab0d60b68ad05f44cf221b3873f5e6af678c096
Ali Fakhry <ali.fakhry@fresh.com.eg>
2020-08-30 10:48:55 +0200

    Finished Vuex Module but didn't Evolved the Module Strategy to it

--- src/components/Manipluate.vue
+++ src/components/Manipluate.vue
@@ -2,28 +2,31 @@
   <div class="container-fluid">
     <div class="row py-3">
       <div class="col-sm-6 offset-sm-3">
-        <button class="btn btn-success w-50" @click="increaseCount">Increase</button>
-        <button class="btn btn-danger w-50" @click="decreaseCount">Decrease</button>
+        <button class="btn btn-success w-50" @click="increase">Increase</button>
+        <button class="btn btn-danger w-50" @click="decreament">Decrease</button>
       </div>
       <div class="col-sm-6 offset-sm-3">
-        <button class="btn btn-warning w-75 mx-auto btn-block" @click="resetCount">Reset</button>
+        <button class="btn btn-warning w-75 mx-auto btn-block" @click="resetAll">Reset</button>
+      </div>
+      <div class="col-sm-6 offset-sm-3">
+        <hr />
+        <button class="btn btn-success w-50" @click="asyncIncrease">Async Increase</button>
+        <button
+          class="btn btn-danger w-50"
+          @click="decreaseByAmount({by: 5, time: 500})"
+        >Decrease By 5 Within Time</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapMutations, mapActions } from "vuex";
+
 export default {
   methods: {
-    increaseCount() {
-      this.$store.state.count += 1;
-    },
-    decreaseCount() {
-      this.$store.state.count -= 1;
-    },
-    resetCount() {
-      this.$store.state.count = 0;
-    },
+    ...mapMutations(["increament", "decreament", "resetAll"]),
+    ...mapActions(["increase", "asyncIncrease", "decreaseByAmount"]),
   },
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -11,6 +11,34 @@ const store = new Vuex.Store({
         exactCount: state => state.count,
         doubleCount: state => state.count * 2,
         oddOrEven: state => state.count % 2 ? 'odd' : 'even'
+    },
+    mutations: {
+        // NEVER-EVER :  Mutate States Asynchronsouly .
+        increament: state => {
+            state.count += 1;
+        },
+        decreament: state => {
+            state.count -= 1;
+        },
+        decreamentTwo: (state, payload) => {
+            state.count -= payload.by
+        },
+        resetAll: state => {
+            state.count = 0;
+        }
+    },
+    actions: {
+        increase: ({ commit }) => {
+            commit('increament');
+        },
+        // The Solution to USE Asynchronousity into VUEX .
+        asyncIncrease: ({ commit }) => {
+            setTimeout(() => commit('increament'), 2000)
+        },
+        // Adding Payload
+        decreaseByAmount: ({ commit }, payload) => {
+            setTimeout(() => commit('decreamentTwo', payload), payload.time)
+        }
     }
 });
 
